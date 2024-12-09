18cf7eebf40426d0fb30c80d3709bdafe619506a
Aaron Byrne <aaronbyrne@Aarons-MBP.lan>
2024-12-09 00:38:22 -0500

    Initial Commit

---
+++ MyMotivationPal/MyMotivationPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMotivationPalApp.swift
+//  MyMotivationPal
+//
+//  Created by Aaron Byrne on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMotivationPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
